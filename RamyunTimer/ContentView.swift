f52b51b5096ee5788ed8bfe6a9ce947c2c53649d
ebcho <joeunbi514@naver.com>
2023-01-17 23:10:54 +0900

    initial commit

--- RamyunTimer/ContentView.swift
+++ RamyunTimer/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                Spacer()
+                Text("라면 타이머")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .padding(.vertical, 40.0)
+                Spacer()
+                VStack{
+                    Spacer()
+                    HStack {
+                        VStack{
+                            Image("ramyun1")
+                                .resizable().aspectRatio(contentMode:.fit).padding(.all)
+                                Text("신라면")
+                        }
+                        VStack {
+                            Image("ramyun2")
+                                .resizable()
+                                .aspectRatio(contentMode:.fit)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            Text("진라면")
+                        }
+                    }
+                    Spacer()
+                    HStack {
+                        VStack{
+                            Image("ramyun3")
+                                .resizable()
+                                .aspectRatio(contentMode:.fit)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            Text("짜파게티")
+                        }
+                        VStack {
+                            Image("ramyun4")
+                                .resizable()
+                                .aspectRatio(contentMode:.fit)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            Text("안성탕면")
+                        }
+                    }
+                    Spacer()
+                    HStack {
+                        VStack{
+                            Image("ramyun5")
+                                .resizable()
+                                .aspectRatio(contentMode:.fit)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            Text("불닭볶음면")
+                        }
+                        VStack {
+                            Image("ramyun6")
+                                .resizable()
+                                .aspectRatio(contentMode:.fit)
+                                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            Text("팔도비빔면")
+                        }
+                    }
+                    Spacer()
+
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
---
+++ RamyunTimer/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  RamyunTimer
+//
+//  Created by Eunbi Cho on 2023/01/17.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
