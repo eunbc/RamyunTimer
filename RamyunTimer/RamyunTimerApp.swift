66423c66d826b4348cb701313f4307a2f7c4f42d
ebcho <joeunbi514@naver.com>
2023-01-17 11:55:26 +0900

    Initial Commit

---
+++ RamyunTimer/RamyunTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamyunTimerApp.swift
+//  RamyunTimer
+//
+//  Created by Eunbi Cho on 2023/01/17.
+//
+
+import SwiftUI
+
+@main
+struct RamyunTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
